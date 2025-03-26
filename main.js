8f9e681a8599ba0bc8cc9b2c918b4a11d62cb69b
IanneEuniceValerio <202410968@fit.edu.ph>
2025-03-27 05:39:51 +0800

    homepage done

--- main.js
+++ main.js
@@ -41,10 +41,32 @@ if(loginClose){
 }
 
 /*=============== ADD SHADOW HEADER ===============*/
-
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                        : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
 
 /*=============== HOME SWIPER ===============*/
+let swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: -24,
+    grabCursor: true,
+    slidesPerView:'auto',
+    centeredSlides: 'auto',
 
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        1120: {
+            spaceBetween: -32,
+        }
+    }
+  });
 
 /*=============== FEATURED SWIPER ===============*/
 
